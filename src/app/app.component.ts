691f5b56dd074b73afd278577be7b3fd8fb53d36
Cida Luna <cidalunna@hotmail.com>
2023-12-12 21:53:03 -0300

    created card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, CardComponent]
 })
 export class AppComponent {
   data = {
-    title: 'Angular Course with Vasco';
+    title: 'Angular Course with Vasco',
   }
 
   onLogoClicked(){
