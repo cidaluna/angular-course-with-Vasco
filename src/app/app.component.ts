c4567a2ec41a10d1063e40bb92588e25f9ba14b3
Cida Luna <cidalunna@hotmail.com>
2023-12-15 14:15:56 -0300

    review title and properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { COURSES } from '../../db-data';
 import { CardComponent } from "./card/card.component";
 
 @Component({
@@ -14,6 +15,11 @@ export class AppComponent {
   data = {
     title: 'Angular Course with Vasco',
   }
+
+
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
 
   onLogoClicked(){
     alert("The logo was clicked!")
